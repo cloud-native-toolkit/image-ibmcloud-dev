9e5ce63c39cbbb1e0fd2cf29c9a3c855220a3d53
Sean Sundberg <seansund@us.ibm.com>
2019-05-20 09:39:44 -0500

    Adds image_tag option to scripts. Split out build and push scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="$1"
+IMAGE_VER="$2"
+IMAGE_TAG="$3"
+
+if [[ -z "${IMAGE_NAME}" ]] || [[ -z "${IMAGE_VER}" ]]; then
+  echo "Usage: $0 {REGISTRY_NAME{IMAGE_NAME} {IMAGE_VER} [{IMAGE_TAG}]"
+  exit 1
+fi
+
+if [[ -n "${IMAGE_TAG}" ]]; then
+  IMAGE_VER="${IMAGE_VER}-${IMAGE_TAG}"
+fi
+
+docker build -t ${IMAGE_NAME}:${IMAGE_VER} .
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,35 +2,24 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-IMAGE_NAME="$1"
-IMAGE_VER="$2"
-REGISTRY_NAME="$3"
+REGISTRY_NAME="$1"
+IMAGE_NAME="$2"
+IMAGE_VER="$3"
+IMAGE_TAG="$4"
 
-if [[ -z "${IMAGE_NAME}" ]]; then
-  echo "IMAGE_NAME for the Docker image is required as the first argument"
+if [[ -z "${REGISTRY_NAME}" ]] || [[ -z "${IMAGE_NAME}" ]] || [[ -z "${IMAGE_VER}" ]]; then
+  echo "Usage: $0 {REGISTRY_NAME} {IMAGE_NAME} {IMAGE_VER} [{IMAGE_TAG}]"
   exit 1
 fi
 
-if [[ -z "${IMAGE_VER}" ]]; then
-  echo "IMAGE_VERSION is required as the second argument"
-  exit 1
+if [[ -n "${IMAGE_TAG}" ]]; then
+  IMAGE_VER="${IMAGE_VER}-${IMAGE_TAG}"
 fi
 
-if [[ -z "${REGISTRY_NAME}" ]]; then
-  echo "REGISTRY_NAME for Docker Registry is required as the third argument"
-  exit 1
-fi
+docker tag "${IMAGE_NAME}:${IMAGE_VER}" "${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}"
+docker push "${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}"
 
-if [[ -n $(docker image ls ${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}) ]]; then
-  echo -e "The image \033[1;32m${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}\033[0m already exists"
-  echo -en "Do you want to (o)verwrite the existing image or e(x)it? \033[1;33m[x] | o?\033[0m "
-  read option
-  if [[ "${option}" == "o" ]] || [[ "${option}" == "O" ]]; then
-    echo -e "You chose to \033[1;31moverwrite\033[0m, proceeding with push"
-  else
-    echo -e "Exiting... Run \033[1;32mnpm version [option]\033[0m to update the version and initiate the push"
-    exit 0
-  fi
+if [[ -z "${IMAGE_TAG}" ]]; then
+  docker tag "${IMAGE_NAME}:${IMAGE_VER}" "${REGISTRY_NAME}/${IMAGE_NAME}:latest"
+  docker push "${REGISTRY_NAME}/${IMAGE_NAME}:latest"
 fi
-
-${SCRIPT_DIR}/build-kube.sh ${REGISTRY_NAME} ${IMAGE_NAME} ${IMAGE_VER}
